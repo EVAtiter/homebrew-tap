ebdcf7bf8e929139705944306f4d0d79a326215c
EVAtiter <febata@gmail.com>
2026-06-18 13:09:50 +0900

    eva-clock-rebirth 2.9.6 (新規 cask 追加)

---
+++ Casks/eva-clock-rebirth.rb
@@ -0,0 +1,23 @@
+cask "eva-clock-rebirth" do
+  version "2.9.6"
+  sha256 "72cfcb065c04fe378fd6cb4ead895a95cb60eb4a1d86ddba9a4cf86e7fabfafd"
+
+  url "https://github.com/EVAtiter/EVA-Clock-Rebirth/releases/download/v#{version}/EVA-Clock-Rebirth-#{version}.zip"
+  name "EVA Clock:Rebirth"
+  desc "NERV-style desktop clock with CPU / GPU / ANE meters"
+  homepage "https://github.com/EVAtiter/EVA-Clock-Rebirth"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :ventura
+
+  app "EVA Clock Rebirth.app"
+
+  zap trash: [
+    "~/Library/Caches/com.slack-kingdom.EVAClock",
+    "~/Library/Preferences/com.slack-kingdom.EVAClock.plist",
+  ]
+end
