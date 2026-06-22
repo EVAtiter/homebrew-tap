df8ed50166d3667fa2d02abcd21e0a78912c5c76
EVAtiter <febata@gmail.com>
2026-06-22 14:22:15 +0900

    Add gigant-saver 1.0.4

---
+++ Casks/gigant-saver.rb
@@ -0,0 +1,22 @@
+cask "gigant-saver" do
+  version "1.0.4"
+  sha256 "d8e0eec680bf776f1066428c63df3c041b71e12fcbc550c03a33b4b05fbe528b"
+
+  url "https://github.com/EVAtiter/gigant-saver-release/releases/download/v#{version}/GigantSaver-#{version}.zip"
+  name "GigantSaver"
+  desc "Retro-futuristic liquid meter screen saver for macOS"
+  homepage "https://github.com/EVAtiter/gigant-saver-release"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :sonoma
+
+  screen_saver "GigantSaver.saver"
+
+  zap trash: [
+    "~/Library/Containers/com.apple.wallpaper.agent/Data/Library/Caches/com.apple.wallpaper.caches/screenSaver-",
+  ]
+end
