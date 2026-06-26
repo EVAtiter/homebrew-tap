ee8426aefc5e91645c0db4368c27a7ffc93c19bf
EVAtiter <febata@gmail.com>
2026-06-26 20:31:15 +0900

    gigant-monitor-pro 2.0.3 (new cask)

---
+++ Casks/gigant-monitor-pro.rb
@@ -0,0 +1,24 @@
+cask "gigant-monitor-pro" do
+  version "2.0.3"
+  sha256 "fdb34efbd473a5eef39f7c2713f23088ebc67345d23ef9759f26f997e5c29fe8"
+
+  url "https://github.com/EVAtiter/gigant-monitor-pro-release/releases/download/v#{version}/Gigant-Monitor-Pro-#{version}.zip"
+  name "Gigant Monitor Pro"
+  desc "Retro-futuristic liquid system meters for the menu bar"
+  homepage "https://github.com/EVAtiter/gigant-monitor-support"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :sonoma
+
+  app "Gigant Monitor Pro.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.slack-kingdom.GigantMonitor.pro.plist",
+    "~/Library/Caches/com.slack-kingdom.GigantMonitor.pro",
+    "~/Library/Saved Application State/com.slack-kingdom.GigantMonitor.pro.savedState",
+  ]
+end
