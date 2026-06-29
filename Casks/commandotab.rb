fb5709ed956e28ec4ad516485dc3edb57ea5934d
EVAtiter <febata@gmail.com>
2026-06-29 22:07:45 +0900

    commandotab 1.1.9 (arm64 only: add depends_on arch: :arm64)

--- Casks/commandotab.rb
+++ Casks/commandotab.rb
@@ -1,6 +1,6 @@
 cask "commandotab" do
-  version "1.1.8"
-  sha256 "1d1629ad96671b61df3cf0d71aa37ecaf499b5784173bff4b205486e6bc05c93"
+  version "1.1.9"
+  sha256 "0f34900cc6ceff6387f8e2f44a0d392b26e46d0cecea164810742cb5dd8efa70"
 
   url "https://github.com/EVAtiter/CommandoTab-release/releases/download/v#{version}/CommandoTab-#{version}.zip"
   name "CommandoTab"
@@ -12,6 +12,7 @@ cask "commandotab" do
     strategy :github_latest
   end
 
+  depends_on arch: :arm64
   depends_on macos: :ventura
 
   app "CommandoTab.app"
