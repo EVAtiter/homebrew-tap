1cc196802b9fbff3dc57c78f70d23b1e0fcaa84a
EVAtiter <febata@gmail.com>
2026-07-02 11:39:48 +0900

    fuel-level-plus 2.0.0

---
+++ Casks/fuel-level-plus.rb
@@ -0,0 +1,25 @@
+cask "fuel-level-plus" do
+  version "2.0.0"
+  sha256 "fa61e2047ebd783fe67e266278317339b42814c6e07297bb2e6a7a4bb184b453"
+
+  url "https://github.com/EVAtiter/fuel-level-release/releases/download/v#{version}/Fuel-Level-Plus-#{version}.zip"
+  name "Fuel Level Plus"
+  desc "Menu bar fuel gauge for free memory, plus a battery thermometer"
+  homepage "https://github.com/EVAtiter/fuel-level-release"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on arch: :arm64
+  depends_on macos: :sonoma
+
+  app "Fuel Level Plus.app"
+
+  zap trash: [
+    "~/Library/Caches/com.slack-kingdom.fuellevel.plus",
+    "~/Library/Preferences/com.slack-kingdom.fuellevel.plus.plist",
+    "~/Library/Saved Application State/com.slack-kingdom.fuellevel.plus.savedState",
+  ]
+end
