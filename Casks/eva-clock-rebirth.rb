6de9213cac4c590708b7c9ac6e208c7c5e4eb5df
EVAtiter <febata@gmail.com>
2026-07-04 21:00:48 +0900

    eva-clock-rebirth 2.9.8

--- Casks/eva-clock-rebirth.rb
+++ Casks/eva-clock-rebirth.rb
@@ -1,6 +1,6 @@
 cask "eva-clock-rebirth" do
-  version "2.9.7"
-  sha256 "a02fdefd5bf79258b1f544060a8a3033da47d51fb9cea54189e422858a5d4589"
+  version "2.9.8"
+  sha256 "ee40b8052af6ff26c3344323e0c06df10fd7c9f0a5bdd8896030fdf397c324ab"
 
   url "https://github.com/EVAtiter/EVA-Clock-Rebirth/releases/download/v#{version}/EVA-Clock-Rebirth-#{version}.zip"
   name "EVA Clock:Rebirth"
