c466eab209c5f615f014b5cad5eae1d77d0935b6
EVAtiter <febata@gmail.com>
2026-07-12 09:07:48 +0900

    commandotab 1.1.12

--- Casks/commandotab.rb
+++ Casks/commandotab.rb
@@ -1,6 +1,6 @@
 cask "commandotab" do
-  version "1.1.11"
-  sha256 "d13be1de2cfbfe77c6ee6b96397659f249c8e221d2145d1e81f2652f90c336b5"
+  version "1.1.12"
+  sha256 "d5ba2ee17610880018f9b1d048f5aa5093622dcafa4074860083c6dd29b0a672"
 
   url "https://github.com/EVAtiter/CommandoTab-release/releases/download/v#{version}/CommandoTab-#{version}.zip"
   name "CommandoTab"
