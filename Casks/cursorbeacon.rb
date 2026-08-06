3f8dafb23fea5ae473e8a61953cbf036b44b2b09
EVAtiter <febata@gmail.com>
2026-08-07 01:10:44 +0900

    cursorbeacon 1.1.0

--- Casks/cursorbeacon.rb
+++ Casks/cursorbeacon.rb
@@ -1,10 +1,10 @@
 cask "cursorbeacon" do
-  version "1.0.0"
-  sha256 "2cee8da449a3f2a08510d577959fa8226338fefc2fa6314471a05f9e77f40100"
+  version "1.1.0"
+  sha256 "5538258595e7dd5fd1aac6eb1a96d8174b1fdb05c36905351cd298c3d07bc6a9"
 
   url "https://github.com/EVAtiter/CursorBeacon-release/releases/download/v#{version}/CursorBeacon-#{version}.zip"
   name "CursorBeacon"
-  desc "Enlarges the mouse cursor while the right Option key is held"
+  desc "Enlarges the mouse cursor while the fn or right Option key is held"
   homepage "https://github.com/EVAtiter/CursorBeacon-release"
 
   livecheck do
