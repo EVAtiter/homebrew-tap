2d6c1221b18f5a74b46a705d0c0d722e499ba5d5
EVAtiter <febata@gmail.com>
2026-08-30 11:15:39 +0900

    gigant-monitor-plus 2.4.1

--- Casks/gigant-monitor-plus.rb
+++ Casks/gigant-monitor-plus.rb
@@ -1,6 +1,6 @@
 cask "gigant-monitor-plus" do
-  version "2.4.0"
-  sha256 "ad8a31304be1bf008fe5950b9f614c3c6537786fe1e9abeeca47ef815a283ee5"
+  version "2.4.1"
+  sha256 "21a58eecd42f9908ef5ec663d3c050ec432e6d31e139cbd173bf7ead253f4cb1"
 
   url "https://github.com/EVAtiter/gigant-monitor-pro-release/releases/download/v#{version}/Gigant-Monitor-Plus-#{version}.zip"
   name "Gigant Monitor Plus"
