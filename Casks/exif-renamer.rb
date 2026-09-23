647657a67732f31b96d3f6477816ba3f915bf43c
EVAtiter <febata@gmail.com>
2026-09-23 16:01:50 +0900

    exif-renamer 1.3.0

--- Casks/exif-renamer.rb
+++ Casks/exif-renamer.rb
@@ -1,6 +1,6 @@
 cask "exif-renamer" do
-  version "1.2.0"
-  sha256 "d64b7f6bc5917d93dee4bb46ec7de11ee06aa553c07e7aaa3acd864e29ed0a77"
+  version "1.3.0"
+  sha256 "6778a3f2e0e49550d1e7f579e62c9047ee8472e369b932509e5726a140887197"
 
   url "https://github.com/EVAtiter/exif-renamer-release/releases/download/v#{version}/Exif-Renamer-#{version}.zip"
   name "Exif-Renamer"
@@ -18,6 +18,7 @@ cask "exif-renamer" do
   depends_on macos: :ventura
 
   app "Exif-Renamer.app"
+  binary "exif-renamer"
 
   zap trash: [
     "~/Library/Preferences/com.slack-kingdom.exifrenamer.plist",
